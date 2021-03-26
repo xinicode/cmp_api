ebf91dc824018d09c056d31f332f43903583104c
xini <timcook751@yaoo.com>
2021-03-26 16:52:10 +0800

    bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  title: 'CMP前端API文档',  // 设置网站标题
-  description: 'api',
+  title: 'CMP前端API文档',  
+  description: '',
   base: '/',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/favicon.ico' }], 
   ],
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
   themeConfig: {
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-    lastUpdated: 'Last Updated' // 文档更新时间：每个文件git最后提交的时间
-  },
-  themeConfig: {
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     nav: [
       { text: '网站', link: 'http://10.202.34.147/?cmproot=bsip&_tocmp=1e' },
-      { text: '接口字段定义', link: '/api' },
+      { text: 'iview', link: 'https://docs.bingosoft.net/bingo/components/#/adminpro/introduce' },
     ],
     sidebar: {
     
--- docs/README.md
+++ docs/README.md
@@ -4,11 +4,11 @@ heroImage: /logo.webp
 actionText: 快速上手 →
 actionLink: /api/api.md
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 通用组件
+  details: 通用删除、导出Excel等。
+- title: 通用方法
+  details: 字符串筛选、URL截取、单位转换等。
+- title: UI组件规范
+  details: UI组件集合。
+footer: MIT Licensed | Copyright © 2021-present cuihao
 ---
